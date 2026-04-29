bcdc19d5b8d748dffd046697de2d97f8f1afca30
NeavPanjwani56 <Neav.Panjwani.5062582@ves.ac.in>
2026-04-29 15:41:53 +0530

    initialize project assets, vendor libraries, and login authentication page

---
+++ Rentteez/auth/login.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <meta charset="utf-8" />
+    <title>Log In | Rentteez</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Rentteez - Modern Rental Ecosystem" name="description" />
+    <meta content="Rentteez" name="author" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../assets/images/favicon.ico">
+
+    <!-- Theme Config Js -->
+    <script src="../assets/js/config.js"></script>
+
+    <!-- Vendor css -->
+    <link href="../assets/css/vendor.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- App css -->
+    <link href="../assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-style" />
+
+    <!-- Icons css -->
+    <link href="../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <div class="auth-bg d-flex min-vh-100 justify-content-center align-items-center">
+        <div class="row g-0 justify-content-center w-100 m-2">
+            <div class="col-xl-4 col-lg-5 col-md-6">
+                <div class="card overflow-hidden text-center p-3 mb-0">
+                    <a href="index.html" class="auth-brand mb-3">
+                        <img src="../assets/images/Logo/Main-logo-dark.png" alt="dark logo" height="24" class="logo-dark">
+                        <img src="../assets/images/Logo/main-logo-light.png" alt="logo light" height="24" class="logo-light">
+                    </a>
+
+                    <h3 class="fw-semibold mb-2">Login your account</h3>
+
+                    <p class="text-muted mb-3">Enter your email address and password to access admin panel.</p>
+
+                    <div class="d-flex justify-content-center gap-2 mb-2">
+                        <a href="#!" class="btn btn-soft-danger avatar-md"><i class="ti ti-brand-google-filled fs-18"></i></a>
+                        <a href="#!" class="btn btn-soft-success avatar-md"><i class="ti ti-brand-apple fs-18"></i></a>
+                        <a href="#!" class="btn btn-soft-primary avatar-md"><i class="ti ti-brand-facebook fs-18"></i></a>
+                        <a href="#!" class="btn btn-soft-info avatar-md"><i class="ti ti-brand-linkedin fs-18"></i></a>
+                    </div>
+
+                    <p class="fs-13 fw-semibold">Or Login With Email</p>
+
+                    <form action="https://coderthemes.com/osen/layouts/index.html" class="text-start mb-2">
+                        <div class="mb-2">
+                            <label class="form-label" for="example-email">Email</label>
+                            <input type="email" id="example-email" name="example-email" class="form-control" placeholder="Enter your email">
+                        </div>
+
+                        <div class="mb-2">
+                            <label class="form-label" for="example-password">Password</label>
+                            <input type="password" id="example-password" class="form-control" placeholder="Enter your password">
+                        </div>
+
+                        <div class="d-flex justify-content-between mb-3">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="checkbox-signin">
+                                <label class="form-check-label" for="checkbox-signin">Remember me</label>
+                            </div>
+
+                            <a href="auth-recoverpw.html" class="text-muted border-bottom border-dashed">Forget Password</a>
+                        </div>
+
+                        <div class="d-grid">
+                            <button class="btn btn-primary" type="submit">Login</button>
+                        </div>
+                    </form>
+
+                    <p class="text-danger fs-14 mb-2">Don't have an account? <a href="auth-register.html" class="fw-semibold text-dark ms-1">Sign Up !</a></p>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Vendor js -->
+    <script src="../assets/js/vendor.min.js"></script>
+
+    <!-- App js -->
+    <script src="../assets/js/app.js"></script>
+
+</body>
+
+
+
+</html>
